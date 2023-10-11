f6d9bdfc0e8c0a082d5fdf2b3d57668f25d502f9
saritha <sarithacs10@gmail.com>
2023-10-12 00:07:10 +0530

    added project

---
+++ capstoneproject/home/customers/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+from .forms import  UserRegisterForm
+def register(request):
+    if request.method=='POST':
+
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form=UserRegisterForm
+
+    return render (request,"register.html",{ 'form':form})
+
+
+# Create your views here.
